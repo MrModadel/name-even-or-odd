877f008126702fb52157fe8ef45d3000aa9e480d
MrModadel <timanormu@gmail.com>
2023-01-22 17:32:51 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+let user_name = prompt("Как тебя зовут?");
+
+let numder_name = user_name.length;
+if (numder_name % 2 === 0){
+   alert("Welcome to the site!")
+} else {
+   alert ("Log out of the site")
+}
